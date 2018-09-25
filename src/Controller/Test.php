a8d74e082f555be81a2d66452ad065557a73be24
Maxim Postoronca <maxim.postoronca@gmail.com>
2018-09-25 19:55:54 +0200

    added basic autoloader

---
+++ src/Controller/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Defconshop\Controller;
+
+class Test
+{
+    public function hello()
+    {
+        echo "Hello";
+    }
+}
---
+++ src/autoloader.php
@@ -0,0 +1,24 @@
+<?php
+
+class MyAutoloader
+{
+    public static function startAutoloader()
+    {
+        spl_autoload_register(array("MyAutoloader", "loadThis"));
+
+    }
+
+    public static function loadThis($className)
+    {
+        $class_name = str_replace("\\", DIRECTORY_SEPARATOR, $className);
+        $class_name = str_replace("Defconshop", "", $class_name);//TODO add directory mapping
+
+        $directory = dirname(__FILE__);
+        $file_name = $directory . $class_name . ".php";
+
+        if (file_exists($file_name)) {
+            require_once($file_name);
+        }
+    }
+
+}
